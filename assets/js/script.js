1e69506d1a06d08989908a5607c3f7c137fd5048
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-06-17 17:18:32 -0700

    added if statment for field requirements

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,17 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false; 
+}
+    //this isn't resetting both fields like the mock up
+    formEl.reset();
+    
+
       // package up data as an object
-  var taskDataObj = {
+    var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
   };
@@ -16,7 +25,7 @@ var taskFormHandler = function(event) {
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
 
-  }
+}
 
   var createTaskEl = function(taskDataObj) {
      // create list item
